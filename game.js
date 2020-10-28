b9805e94a5fd839404288d763cc6955259b07488
Joyson Puvanasingham <60553801+enochj316@users.noreply.github.com>
2020-10-27 20:10:22 -0400

    JS updates

--- game.js
+++ game.js
@@ -12,46 +12,46 @@ var questionCounter = 0;
 var availableQuestions = []
 
 var questions = [
-{
-    question: "What is 2 + 2",
-    possibleChoices: [
-    "2",
-    "3",
-    "4",
-    "6",
-    ],
-    answer: "4",
-},
-{
-    question: "What is 2 + 6",
-    possibleChoices: [
-    "2",
-    "3",
-    "4",
-    "8",
-    ],
-    answer: "8",
-},
-{
-    question: "What is 2 + 9",
-    possibleChoices: [
-    "2",
-    "3",
-    "4",
-    "11",
-    ],
-    answer: "11",
-},
-{
-    question: "What is 2 + 10",
-    possibleChoices: [
-    "2",
-    "3",
-    "12",
-    "6",
-    ],
-    answer: "12",
-},
+    {
+        question: "What is 2 + 2",
+        possibleChoices: [
+            "2",
+            "3",
+            "4",
+            "6",
+        ],
+        answer: "4",
+    },
+    {
+        question: "What is 2 + 6",
+        possibleChoices: [
+            "2",
+            "3",
+            "4",
+            "8",
+        ],
+        answer: "8",
+    },
+    {
+        question: "What is 2 + 9",
+        possibleChoices: [
+            "2",
+            "3",
+            "4",
+            "11",
+        ],
+        answer: "11",
+    },
+    {
+        question: "What is 2 + 10",
+        possibleChoices: [
+            "2",
+            "3",
+            "12",
+            "6",
+        ],
+        answer: "12",
+    },
 
 ]
 
@@ -62,49 +62,56 @@ const startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-    getNewQuestion ()
+    getNewQuestion()
 }
 
 
 const getNewQuestion = () => {
-if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS)
-{localStorage.setItem("mostRecentScore", score)
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem("mostRecentScore", score)
 
-    return window.location.assign("/end.html")
+        return window.location.assign("/end.html")
+    }
+
+    questionCounter++
+    progressText.innerText = `Questions ${questionCounter} of ${MAX_QUESTIONS}`
+    // progressBarFull.style.width =`${(questionCounter/MAX_QUESTIONS) * 100}%`
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach((choice, index) => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion.possibleChoices[index]
+        choice.style.background = ""
+        choice.addEventListener("click", answerClick)
+    })
+
+    availableQuestions.splice(questionsIndex, 1);
+
+    acceptingAnswers = true;
+
 }
 
-questionCounter++
-progressText.innerText = `Questions ${questionCounter} of ${MAX_QUESTIONS}`
-// progressBarFull.style.width =`${(questionCounter/MAX_QUESTIONS) * 100}%`
+function answerClick(evt) {
+    var correctAnswer = currentQuestion.answer;
+// console.log(evt);
 
-const questionsIndex = Math.floor(Math.random() *availableQuestions.length)
-currentQuestion = availableQuestions[questionsIndex]
-question.innerText = currentQuestion.question
+    if (correctAnswer == this.textContent) {
+    
+        evt.target.style.background= "green";
+    }
+    else {
 
-choices.forEach((choice, index) => {
-    const number = choice.dataset['number']
-    choice.innerText = currentQuestion.possibleChoices[index]
-    choice.addEventListener("click",answerClick)
-})
+        evt.target.style.background= "red";
+   
+    }
+        setTimeout(() => {
+            getNewQuestion();
+        }, 1000);
 
-availableQuestions.splice(questionsIndex, 1)
-
-acceptingAnswers = true
-
-}
-
-function answerClick ()
-{ 
-    var correctAnswer = currentQuestion.answer
-  
-    if ( correctAnswer == this.textContent)
-
-alert("Correct")
-
-else 
-alert("Incorrect")
-
-getNewQuestion()
+    
 }
 
 startGame()
